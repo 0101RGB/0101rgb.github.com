2949ec2ca9d4b6e3afdc1a492702dbf5b932ec9a
godjaku <0101rgb@gmail.com>
2016-05-28 23:35:33 +0900

    changed 160528, modify subtitle view

--- js/translate.js
+++ js/translate.js
@@ -2,16 +2,15 @@ var selected= 0, isSyncing= false;
 var tempTimeInterval, syncTimeInterval, syncTemp;
 var pressedKeys= {};
 var dummytext= ["안녕하세요", "이거슨", "더미데이터를 위한", "테스트 텍스트입니다", "뾰로롱"];
-var dummystart= [0.38240, 5.90195, 7.55261, 9.62738, 13.13370];
-var dummyend= [1.59912, 7.55115, 9.62594, 11.41120, 13.67033];
+var dummystart= [0.3824, 5.9019, 7.5526, 9.6273, 13.133];
+var dummyend= [1.5991, 7.5511, 9.6259, 11.411, 13.670];
 
 window.onload= function(){
   var canvas= document.getElementById("backcanvas");
   var width= window.innerWidth;
-  canvas.style.left= (parseInt(window.innerWidth)/2) + 'px';
-  canvas.style.width= (parseInt(window.innerWidth)/2) + 'px';
-  document.getElementById("timeline_center").style.left= (parseInt(window.innerWidth)/2) + 'px';
-  // addField();
+  canvas.style.left= (strToNum(window.innerWidth, "int")/2) + 'px';
+  canvas.style.width= (strToNum(window.innerWidth, "int")/2) + 'px';
+  document.getElementById("timeline_center").style.left= (strToNum(window.innerWidth, "int")/2) + 'px';
   loadOriginal();
 
   var seekBar = document.getElementById("seek-bar");
@@ -88,7 +87,8 @@ function loadOriginal(){
     newsubtitle.setAttribute("id", "subtitlesync");
     newsubtitle.style.backgroundColor= "#b3e3be";
     newsubtitle.style.width= (parseFloat(dummyend[i]) - parseFloat(dummystart[i]))*100*2 + 'px';
-    newsubtitle.style.left= (parseInt(document.getElementById("timeline_center").style.left) - parseFloat(dummystart[i])*100*2) + 'px';
+    // newsubtitle.style.left= (parseInt(document.getElementById("timeline_center").style.left) - parseFloat(dummystart[i])*100*2) + 'px';
+    newsubtitle.style.left= (parseFloat(dummystart[i])*100*2) + 'px';
     // (parseInt(document.getElementById("timeline_center").style.left) - parseInt(document.getElementById("backcanvas").style.left)) + 'px';
     subtitlecontainer.appendChild(newsubtitle);
 
@@ -131,6 +131,9 @@ function keydownevent(event) {
   if(key == 9) {
     event.preventDefault();
     if(video.paused) {
+      // video.currentTime : type : double
+      video.currentTime= strToNum(document.activeElement.nextElementSibling.innerText, "double");
+      // add timeline moving
       video.play();
       tempTimeInterval= setInterval(function() {
         var canvasId= document.getElementById("backcanvas");
@@ -149,8 +152,6 @@ function keydownevent(event) {
   }
   else if(key == 13) {
     var parent= document.activeElement.parentNode;
-    // if(parent.nextElementSibling) parent.nextElementSibling.firstChild.focus();
-    // else
     addField();
   }
   else if(key == 38){
@@ -245,7 +246,7 @@ function setStart(){
   console.log("left : "+tempLeft);
   syncTemp.style.left= tempLeft;
   syncTemp.style.width= '0px';
-  document.activeElement.nextElementSibling.innerText= document.getElementById("mainvideo").currentTime.toFixed(5);
+  document.activeElement.nextElementSibling.innerText= document.getElementById("mainvideo").currentTime.toPrecision(4);
 
   syncTimeInterval= setInterval(function() {
     syncTemp.style.width= (parseInt(syncTemp.style.width)+2)+'px';
@@ -255,7 +256,7 @@ function setStart(){
 function setEnd(){
   isSyncing= false;
   clearInterval(syncTimeInterval);
-  document.activeElement.nextElementSibling.nextElementSibling.innerText= document.getElementById("mainvideo").currentTime.toFixed(5);
+  document.activeElement.nextElementSibling.nextElementSibling.innerText= document.getElementById("mainvideo").currentTime.toPrecision(4);
 }
 
 function addField(){
@@ -267,57 +268,87 @@ function addField(){
   input.setAttribute("class", "subtitle-text");
   input.setAttribute("onkeydown", "keydownevent(event)");
   item.appendChild(input);
-
   var timingstart= document.createElement("span");
   timingstart.setAttribute("class", "subtitle-start");
-  timingstart.innerText= "0.00000";
   timingstart.setAttribute("align", "right");
   item.appendChild(timingstart);
-
   var timingend= document.createElement("span");
   timingend.setAttribute("class", "subtitle-end");
-  timingend.innerText= "0.00000";
   timingend.setAttribute("align", "right");
   item.appendChild(timingend);
-
-  if(countSubtitles() == 0)
-    container.appendChild(item);
-  else{
-    var active= document.activeElement;
-    if(active.nodeName == 'INPUT' || active.nodeName == 'input'){
-      var activeParent= active.parentNode;
-      if(activeParent.nextElementSibling)
-        container.insertBefore(item, activeParent.nextElementSibling);
-      else container.appendChild(item);
-    }
-  }
-  input.focus();
-
+  timingstart.innerText= "0.0000";
+  timingend.innerText= "0.0000";
   var subtitlecontainer= document.getElementById("backcanvas");
   var newsubtitle= document.createElement("div");
   newsubtitle.setAttribute("id", "subtitlesync");
   newsubtitle.style.backgroundColor= "#b3e3be";
-  newsubtitle.style.width= "0px";
   subtitlecontainer.appendChild(newsubtitle);
+
+  if(countSubtitles() == 0){
+      container.appendChild(item);
+      timingstart.innerText= "0.0000";
+      timingend.innerText= "0.0000";
+      newsubtitle.style.width= "0px";
+  }
+  else{
+    var active= document.activeElement;
+    if(active.nodeName == "INPUT" || active.nodeName == "input"){
+      var activeParent= active.parentNode;
+      var aprime= strToNum(active.nextElementSibling.nextElementSibling.innerText, "double");
+      var b= strToNum(activeParent.nextElementSibling.firstChild.nextElementSibling.innerText, "double");
+
+      var divA= getSubtitleFrame(getSubtitleIndex(), "div");
+      var divB= getSubtitleFrame(getSubtitleIndex()+1, "div");
+
+      if(b-aprime < 0.5){
+        var a= strToNum(active.nextElementSibling.innerText, "double");
+        var bprime= strToNum(activeParent.nextElementSibling.firstChild.nextElementSibling.nextElementSibling.innerText, "double");
+        var term= ((bprime-a)/3).toPrecision(5);
+        var termWidth= (strToNum(divB.style.left, "int") + strToNum(divB.style.width, "int") - strToNum(divA.style.left, "int") - strToNum(divA.style.width, "int")) / 3;
+
+        active.nextElementSibling.nextElementSibling.innerText= strToNum(a, "double")+strToNum(term, "double");
+        active.nextElementSibling.nextElementSibling.innerText= strToNum(active.nextElementSibling.nextElementSibling.innerText, "double").toPrecision(5);
+        activeParent.nextElementSibling.firstChild.nextElementSibling.innerText= strToNum(a, "double")+strToNum(term, "double")*2;
+        activeParent.nextElementSibling.firstChild.nextElementSibling.innerText= strToNum(activeParent.nextElementSibling.firstChild.nextElementSibling.innerText, "double").toPrecision(5);
+        timingstart.innerText= strToNum(a, "double")+strToNum(term, "double");
+        timingstart.innerText= strToNum(timingstart.innerText, "double").toPrecision(5);
+        timingend.innerText= strToNum(a, "double")+strToNum(term, "double")*2;
+        timingend.innerText= strToNum(timingend.innerText, "double").toPrecision(5);
+
+        divA.style.width= strToNum(termWidth, "int");
+        divB.style.width= strToNum(termWidth, "int");
+        newsubtitle.style.left= (strToNum(divA.style.left, "int")+strToNum(termWidth, "int")) + "px";
+        newsubtitle.style.width= strToNum(termWidth, "int");
+        divB.style.left= (strToNum(divA.style.left, "int")+strToNum(termWidth, "int")*2) + "px";
+      } else {
+        timingstart.innerText= (strToNum(aprime, "double")).toPrecision(5);
+        timingend.innerText= (strToNum(b, "double")).toPrecision(5);
+        newsubtitle.style.left= (strToNum(divA.style.left, "int") + strToNum(divA.style.width, "int")) + "px";
+        newsubtitle.style.width= (strToNum(divB.style.left, "int") - strToNum(newsubtitle.style.left, "int")) + "px";
+      }
+      if(activeParent.nextElementSibling) container.insertBefore(item, activeParent.nextElementSibling);
+      else container.appendChild(item);
+    }
+  }
+  input.focus();
 }
 
 function overlayText(){ document.getElementById("subtitle-view").innerText= checkTextLength(document.activeElement.value); }
 function checkTextLength(str){
   if(str.length <= 40) return str;
+  if(str.indexOf("(")>0) return str.substring(0, str.indexOf("(")+1)+"\n"+str.substring(str.indexOf("(")+1);
   if(str.indexOf(".")>0) return str.substring(0, str.indexOf(".")+1)+"\n"+str.substring(str.indexOf(".")+1);
   if(str.indexOf("!")>0) return str.substring(0, str.indexOf("!")+1)+"\n"+str.substring(str.indexOf("!")+1);
   if(str.indexOf("?")>0) return str.substring(0, str.indexOf("?")+1)+"\n"+str.substring(str.indexOf("?")+1);
   if(str.indexOf(",")>0) return str.substring(0, str.indexOf(",")+1)+"\n"+str.substring(str.indexOf(",")+1);
-  for(i=20; i<str.length; i++)
-    if(str.indexOf(" ") == i) return str.substring(0, i+1)+"\n"+str.substring(i+1);
+  for(i=20; i<str.length; i++) if(str.indexOf(" ") == i) return str.substring(0, i+1)+"\n"+str.substring(i+1);
 }
 
 function countSubtitles() {
   var ul = document.getElementById("subtitletranslateul");
   var liNodes = [];
   var count= 0;
-  for (var i = 0; i < ul.childNodes.length; i++)
-    if (ul.childNodes[i].nodeName == "LI" || ul.childNodes[i].nodeName == "li") count++;
+  for (var i = 0; i < ul.childNodes.length; i++) if (ul.childNodes[i].nodeName == "LI" || ul.childNodes[i].nodeName == "li") count++;
   return count;
 }
 
@@ -333,4 +364,22 @@ function getSubtitleIndex(a){
   }
   console.log(count);
   return count;
-}
+ }
+
+ // function getDivFromSubtitle(a){
+ //   var ul = document.getElementById("subtitletranslateul");
+ //   var count= 0;
+ //
+ //   for (var i = 0; i < ul.childNodes.length; i++) {
+ //     if (ul.childNodes[i].nodeName == "LI" || ul.childNodes[i].nodeName == "li") {
+ //       if(ul.childNodes[i].firstChild === document.activeElement) break;
+ //       count++;
+ //     }
+ //   }
+ //   console.log(count);
+ //
+ //   return count;
+ // }
+
+
+function strToNum(str, type){ return (type == "double" ? parseFloat(str) : (type == "int" ? parseInt(str) : "")); }
